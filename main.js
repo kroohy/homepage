0f4bfd4411bbc4d9ac08ca7f2be615853686656c
kroohy <kroohy@poczta.onet.pl>
2024-04-25 16:23:48 +0200

    new to js

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstName = "Luke";
+console.log(firstName);
+
+const age = 50;
+console.log(age);
+
+console.log("hello there!");
+console.log("OK");
+
+console.log(`Nazwywam się ${firstName} i mam ${age} lat`);
